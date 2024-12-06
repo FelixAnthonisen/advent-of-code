21b836c6f1b7771d048fa6386a7557a83c9c0521
FelixAnthonisen <felix.anthonisen@gmail.com>
2024-12-06 13:09:50 +0100

    solution day 6

---
+++ 2024/day-6/task1.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <mach-o/dyld.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+string file = "input.txt";
+
+bool inBounds(int y, int x, int N, int M)
+{
+    return y >= 0 && x >= 0 && y < N && x < M;
+}
+
+string getParentPath()
+{
+    char buffer[PATH_MAX];
+    uint32_t size = sizeof(buffer);
+    if (_NSGetExecutablePath(buffer, &size) == 0)
+    {
+        char *lastSlash = strrchr(buffer, '/');
+        if (lastSlash != nullptr)
+            *lastSlash = '\0';
+    }
+    return buffer;
+}
+
+int main()
+{
+    ifstream inputFile(getParentPath() + "/" + file);
+    string line;
+    vector<string> arr;
+    while (getline(inputFile, line))
+        arr.push_back(line);
+    int ans = 1, N = arr.size(), M = arr[0].size(), x, y, dir;
+    vector<char> starts = {'>', 'v', '<', '^'};
+    rep(i, 0, N)
+    {
+        rep(j, 0, M)
+        {
+            char c = arr[i][j];
+            if (find(all(starts), c) != starts.end())
+            {
+                y = i;
+                x = j;
+                auto it = find(all(starts), c);
+                dir = distance(starts.begin(), it);
+            }
+        }
+    }
+    while (1)
+    {
+        x += dir % 2 ? 0 : dir / 2 ? -1
+                                   : 1;
+        y += dir % 2 ? dir / 2 ? -1 : 1 : 0;
+        if (!inBounds(y, x, N, M))
+            break;
+        if (arr[y][x] == '#')
+        {
+            x -= dir % 2 ? 0 : dir / 2 ? -1
+                                       : 1;
+            y -= dir % 2 ? dir / 2 ? -1 : 1 : 0;
+            ++dir;
+            dir %= 4;
+        }
+        else if (arr[y][x] == '.')
+        {
+            arr[y][x] = 'x';
+            ++ans;
+        }
+    }
+    cout << ans;
+}
